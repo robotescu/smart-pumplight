550e7d6b04c82aac9d39e3950b888edd3571d823
robotescu <80849623+robotescu@users.noreply.github.com>
2025-06-16 23:11:31 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,21 @@
 let zgomot = 0
 basic.showIcon(IconNames.Happy)
+let strip = neopixel.create(DigitalPin.P8, 1, NeoPixelMode.RGB)
 basic.pause(2000)
+basic.forever(function () {
+    zgomot = smarthome.ReadNoise(AnalogPin.P10)
+    if (zgomot >= 70) {
+        strip.showColor(neopixel.colors(NeoPixelColors.White))
+        basic.pause(2000)
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    }
+    basic.pause(1000)
+})
 basic.forever(function () {
     if (pins.analogReadPin(AnalogReadWritePin.P1) < 500) {
         pins.digitalWritePin(DigitalPin.P2, 1)
     } else {
         pins.digitalWritePin(DigitalPin.P2, 0)
-    }
-    basic.pause(1000)
-})
-basic.forever(function () {
-    zgomot = smarthome.ReadNoise(AnalogPin.P10)
-    if (zgomot >= 70) {
-        smarthome.ledBrightness(AnalogPin.P8, true)
-        basic.pause(2000)
-        smarthome.ledBrightness(AnalogPin.P8, false)
     }
     basic.pause(1000)
 })
